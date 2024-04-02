dd608f59baf670286245b0928f713b0bffe0c986
caiot <caiot@DESKTOP-42E6AGD>
2024-04-02 20:45:32 -0300

    Atividade feita para treinar o comando Jpanel no java

---
+++ src/main/java/com/mycompany/cad2/Cad2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cad2;
+
+/**
+ *
+ * @author caiot
+ */
+public class Cad2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
